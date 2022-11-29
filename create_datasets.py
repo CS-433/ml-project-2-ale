1d31b281a481045839b8a8fe406bc53c816c247f
lniederhauser <lucille.niederhauser@epfl.ch>
2022-11-29 15:18:18 +0100

    script to create the data sets on the server

---
+++ create_datasets.py
@@ -0,0 +1,11 @@
+from helpers import *
+
+final_data_set_train_wl2_alpha, final_data_set_train_wl2_beta, final_data_set_train_wl2_delta, \
+    final_data_set_train_wl2_gamma, final_data_set_train_wl2_theta, final_data_set_test_wl2_alpha, \
+    final_data_set_test_wl2_beta, final_data_set_test_wl2_delta, final_data_set_test_wl2_gamma, \
+    final_data_set_test_wl2_theta, final_data_set_train_wlog_alpha, final_data_set_train_wlog_beta, \
+    final_data_set_train_wlog_delta, final_data_set_train_wlog_gamma, final_data_set_train_wlog_theta,\
+    final_data_set_test_wlog_alpha, final_data_set_test_wlog_beta, final_data_set_test_wlog_delta, \
+    final_data_set_test_wlog_gamma, final_data_set_test_wlog_theta = load_csv_data_updated('../../Data3/Hamid_ML4Science_ALE/MATLAB/learned_graphs')
+
+print("data sets created")
