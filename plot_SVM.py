3ddbfd60bdbab2d8af1ffd35bcb758c1e60c15aa
Nekoemy <emilie.marcou@epfl.ch>
2022-12-17 00:01:21 +0100

    update plot_SVM

--- plot_SVM.py
+++ plot_SVM.py
@@ -4,6 +4,34 @@
 # -> run on all band at the same time on a given regularisation/C/... and a range of threshold
 
 from models import *
+from sklearn.model_selection import KFold
+def validation(train_setx,train_sety, threshold):
+    kf = KFold(n_splits=5)
+    KFold(random_state=None, shuffle=False)
+    best_threshh=0.0
+    accuracyy=0.0
+    for ind, T in enumerate(threshold):
+        for i, (train_index, test_index) in enumerate(kf.split(train_setx)):
+            valid_trainx=train_setx[train_index]
+            valid_trainy=train_sety[train_index]
+            valid_testx=train_setx[test_index]
+            valid_testy=train_sety[test_index]
+            x_train, x_test = remove_col_lowvariance(pd.DataFrame(valid_trainx), pd.DataFrame(valid_testx), T)
+            accuracy, C, gamma, kernel = SVM_tune_predict_evaluate(x_train, valid_trainy, x_test, valid_testy,
+                                                                   save_path=r'../../../Data3/Hamid_ML4Science_ALE/SVMwithoutVar/plots/',
+                                                                   C_params=[0.1, 1, 10, 100],
+                                                                   gamma_params=[10, 1, 0.1, 0.01, 0.001, 'scale'],
+                                                                   kernel_params=['rbf', 'sigmoid'],
+                                                                   save_fig=False, title="confusion_matrix",
+                                                                   grid_search=False, default_C=1,
+                                                                   default_gamma='scale', default_kernel='rbf')
+            if accuracy>accuracyy:
+                best_threshh = T
+                accuracyy=accuracy
+
+    return best_threshh, accuracyy
+
+
 betas = [0.05, 0.1, 0.15000000000000002, 0.2, 0.25, 0.3, 0.35000000000000003, 0.4, 0.45, 0.5, 0.55, 0.6,
         0.6499999999999999, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.]
 #log
@@ -34,13 +62,15 @@ print("starting predictions")
 for band in bands:
     print("starting predictions for: " + band)
     for j, beta in enumerate(betas):
-        for indice, value in enumerate(threshold_log_all):
-            x_train, y_train = load_data_set(band, reg, "train", beta, path=r'../../../Data3/Hamid_ML4Science_ALE/data_sets/',
+
+
+        x_train, y_train = load_data_set(band, reg, "train", beta, path=r'../../../Data3/Hamid_ML4Science_ALE/data_sets/',
                                                  epochs_combined=True)
-            x_test, y_test = load_data_set(band, reg, "test", beta, path=r'../../../Data3/Hamid_ML4Science_ALE/data_sets/',
+        x_test, y_test = load_data_set(band, reg, "test", beta, path=r'../../../Data3/Hamid_ML4Science_ALE/data_sets/',
                                                epochs_combined=True)
-            x_train, x_test = remove_col_lowvariance(pd.DataFrame(x_train), pd.DataFrame(x_test), value)
-            accuracy, C, gamma, kernel = SVM_tune_predict_evaluate(x_train, y_train, x_test, y_test,
+        best_thresh, accuracy_valid = validation(x_train, y_train,threshold_log_all)
+        x_train, x_test = remove_col_lowvariance(pd.DataFrame(x_train), pd.DataFrame(x_test), best_thresh)
+        accuracy, C, gamma, kernel = SVM_tune_predict_evaluate(x_train, y_train, x_test, y_test,
                                                                                    save_path=r'../../../Data3/Hamid_ML4Science_ALE/SVMwithoutVar/plots/',
                                                                                    C_params=[0.1, 1, 10, 100],
                                                                                    gamma_params=[10, 1, 0.1, 0.01, 0.001, 'scale'],
@@ -48,21 +78,19 @@ for band in bands:
                                                                                    save_fig=False, title="confusion_matrix",
                                                                                    grid_search=False, default_C=1,
                                                                                    default_gamma='scale', default_kernel='rbf')
-            if accuracy_f < accuracy:
 
-                new_row = pd.Series(data={'reg': reg, 'band': band, 'alpha/beta': beta, 'C': C, 'gamma': gamma, 'kernel': kernel,
-                                                      'accuracy': accuracy,'nb_features:': len(pd.DataFrame(x_train).columns)}, name=j)
-                if band == 0:
-                    accuracy_table_al = accuracy_table_al.append(new_row, ignore_index=False)
-                if band == 1:
-                    accuracy_table_be = accuracy_table_be.append(new_row, ignore_index=False)
-                if band == 2:
-                    accuracy_table_de = accuracy_table_de.append(new_row, ignore_index=False)
-                if band == 3:
-                    accuracy_table_ga = accuracy_table_ga.append(new_row, ignore_index=False)
-                if band == 4:
-                    accuracy_table_th = accuracy_table_th.append(new_row, ignore_index=False)
-            print("prediction done with threshold: " + str(value))
+        new_row = pd.Series(data={'reg': reg, 'band': band, 'alpha/beta': beta, 'C': C, 'gamma': gamma, 'kernel': kernel,
+                                                      'accuracy_test': accuracy, 'accuracy_validation':accuracy_valid,'nb_features:': len(pd.DataFrame(x_train).columns)}, name=j)
+        if band == 0:
+            accuracy_table_al = accuracy_table_al.append(new_row, ignore_index=False)
+        if band == 1:
+            accuracy_table_be = accuracy_table_be.append(new_row, ignore_index=False)
+        if band == 2:
+            accuracy_table_de = accuracy_table_de.append(new_row, ignore_index=False)
+        if band == 3:
+            accuracy_table_ga = accuracy_table_ga.append(new_row, ignore_index=False)
+        if band == 4:
+            accuracy_table_th = accuracy_table_th.append(new_row, ignore_index=False)
 
     print("prediction done with beta: " + str(beta))
 
