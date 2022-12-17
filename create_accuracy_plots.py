411da269817efb31d24988ea61f020fc8961ecd8
lniederhauser <lucille.niederhauser@epfl.ch>
2022-12-17 15:20:16 +0100

    Plots all models per band

--- create_accuracy_plots.py
+++ create_accuracy_plots.py
@@ -1,15 +1,20 @@
 from plots import *
 
 # pour run: télécharger sur votre ordi les dossiers RandomForest, SVM_updated_GS et correlations qui sont dans /Data3
-# sur les serveurs, remplacer les tables_path_ par le chemin à ces dossiers sur votre ordi et le saving_path par le
-# dossier où vous voulez les sauver
+# sur les serveurs. Placer RandomForest et correlations dans un dossier (Sont utilisés dans le plot par bande, on pourra
+# peut-être rajouter SVM plus tard si ça donne des résultats consistants). Remplacer tables_path_all par le chemin du
+# dossier contenant RF et correlations et les autres tables_path par les chemin à ces dossiers sur votre ordi
+# et le saving_path par le dossier où vous voulez les sauver
 
-tables_path_RF = r'C:\Users\lucil\Desktop\RandomForest'
+tables_path_RF = r'C:\Users\lucil\Desktop\results\RandomForest'
 tables_path_SVM = r'C:\Users\lucil\Desktop\SVM_updated_GS'
-tables_path_corr = r'C:\Users\lucil\Desktop\correlations'
+tables_path_corr = r'C:\Users\lucil\Desktop\results\correlations'
+tables_path_all = r'C:\Users\lucil\Desktop\results'
 saving_path = r'plots/'
 y_scale_RF_all_epochs = np.linspace(0.35, 1, 14, endpoint=True)
 y_scale_RF = np.linspace(0.8, 1, 9, endpoint=True)
+bands = ["alpha", "beta", "delta", "gamma", "theta"]
+
 
 # plot RF
 plot_accuracies_wrt_parameter(tables_path_RF, saving_path, model_name="Random Forest", regularization="l2", 
@@ -27,11 +32,15 @@ plot_accuracies_wrt_parameter(tables_path_SVM, saving_path, model_name="SVM", re
 plot_accuracies_wrt_parameter_from1file(tables_path_SVM, saving_path, "SVM", "l2", save_fig=True)
 plot_accuracies_wrt_parameter_from1file(tables_path_SVM, saving_path, "SVM", "log", save_fig=True)
 
-#plot benchmark
+# plot benchmark
 plot_accuracies_wrt_parameter(tables_path_corr, saving_path, "Correlations", "l2", save_fig=True, y_scale=y_scale_RF)
 plot_accuracies_wrt_parameter(tables_path_corr, saving_path, "Correlations", "log", save_fig=True, y_scale=y_scale_RF)
 plot_accuracies_wrt_parameter(tables_path_corr, saving_path, "Correlations_all_epochs", "l2", save_fig=True,
                               y_scale=y_scale_RF_all_epochs, all_epochs=True)
 plot_accuracies_wrt_parameter(tables_path_corr, saving_path, "Correlations_all_epochs", "log", save_fig=True,
                               y_scale=y_scale_RF_all_epochs, all_epochs=True)
+
+# plot all models per band
+for band in bands:
+    plot_accuracies_all_settings_1band(tables_path_all, saving_path, band, y_scale=y_scale_RF, save_fig=True)
 
--- plots.py
+++ plots.py
@@ -30,103 +30,8 @@ def plot_confusion_matrix(y_test, y_pred, accuracy, save_path, save_fig=False, t
     else:
         plt.show()
 
-'''
-def plot_accuracies_wrt_parameter_per_band(path_tables, save_path, save_fig=False):
-    path = path_tables
-    all_files = Path(path).glob('*.csv')
-    # files = glob.glob(path, recursive=true)
-    for file in all_files:
-        df = pd.read_csv(file)
-        name_of_file = os.path.basename(os.path.normpath(file))
-        title = name_of_file[:-4]
 
-        if "all_epochs" in title:
-            if "l2" in title:
-                alphas = df["alpha/beta"]
-                alphas = alphas.truncate(after=19)
-                alphas = alphas.to_numpy()
-                alpha_accuracy = df.loc[df["band"] == "alpha"]
-                alpha_accuracy = alpha_accuracy['accuracy']
-                alpha_accuracy = alpha_accuracy.to_numpy()
-                beta_accuracy = df.loc[df["band"] == "beta"]
-                beta_accuracy = beta_accuracy['accuracy']
-                beta_accuracy = beta_accuracy.to_numpy()
-                delta_accuracy = df.loc[df["band"] == "delta"]
-                delta_accuracy = delta_accuracy['accuracy']
-                delta_accuracy = delta_accuracy.to_numpy()
-                gamma_accuracy = df.loc[df["band"] == "gamma"]
-                gamma_accuracy = gamma_accuracy['accuracy']
-                gamma_accuracy = gamma_accuracy.to_numpy()
-                theta_accuracy = df.loc[df["band"] == "theta"]
-                theta_accuracy = theta_accuracy['accuracy']
-                theta_accuracy = theta_accuracy.to_numpy()
-
-                plt.plot(alphas, alpha_accuracy, label='alpha')
-                plt.plot(alphas, beta_accuracy, label='beta')
-                plt.plot(alphas, delta_accuracy, label='delta')
-                plt.plot(alphas, gamma_accuracy, label='gamma')
-                plt.plot(alphas, theta_accuracy, label='theta')
-                plt.legend()
-
-                plt.title(title + ": plot")
-                plt.xlabel('alpha')
-                plt.ylabel('accuracy')
-                if save_fig:
-                    plt.savefig(save_path + title + ".png")
-                else:
-                    plt.show()
-
-            if "log" in title:
-                alphas = df["alpha/beta"]
-                alphas = alphas.truncate(after=19)
-                alphas = alphas.to_numpy()
-                alpha_accuracy = df.loc[df["band"] == "alpha"]
-                alpha_accuracy = alpha_accuracy['accuracy']
-                alpha_accuracy = alpha_accuracy.to_numpy()
-                beta_accuracy = df.loc[df["band"] == "beta"]
-                beta_accuracy = beta_accuracy['accuracy']
-                beta_accuracy = beta_accuracy.to_numpy()
-                delta_accuracy = df.loc[df["band"] == "delta"]
-                delta_accuracy = delta_accuracy['accuracy']
-                delta_accuracy = delta_accuracy.to_numpy()
-                gamma_accuracy = df.loc[df["band"] == "gamma"]
-                gamma_accuracy = gamma_accuracy['accuracy']
-                gamma_accuracy = gamma_accuracy.to_numpy()
-                theta_accuracy = df.loc[df["band"] == "theta"]
-                theta_accuracy = theta_accuracy['accuracy']
-                theta_accuracy = theta_accuracy.to_numpy()
-
-                plt.plot(alphas, alpha_accuracy, label='alpha')
-                plt.plot(alphas, beta_accuracy, label='beta')
-                plt.plot(alphas, delta_accuracy, label='delta')
-                plt.plot(alphas, gamma_accuracy, label='gamma')
-                plt.plot(alphas, theta_accuracy, label='theta')
-                plt.legend()
-
-                plt.title(title + ": plot")
-                plt.xlabel('beta')
-                plt.ylabel('accuracy')
-                if save_fig:
-                    plt.savefig(save_path + title + ".png")
-                else:
-                    plt.show()
-
-        else:
-            df.plot(x='alpha/beta', y='accuracy', kind='line')
-            plt.title(title + ": plot")
-            plt.ylabel('accuracy')
-            if "l2" in title:
-                plt.xlabel('alpha')
-            elif "log" in title:
-                plt.xlabel('beta')
-
-            if save_fig:
-                plt.savefig(save_path + title + ".png")
-            else:
-                plt.show()'''
-
-
-def find_labels(filename):
+def find_band_labels(filename):
     if "alpha" in filename:
         return "alpha"
     elif "beta" in filename:
@@ -139,13 +44,60 @@ def find_labels(filename):
         return "theta"
 
 
+def find_reg_labels(filename):
+    if "correlations" in filename:
+        if "l2" in filename:
+            return "dashed", "benchmark l2"
+        elif "log" in filename:
+            return "dotted", "benchmark log"
+    else:
+        if "l2" in filename:
+            return "solid", "RandomForest l2"
+        elif "log" in filename:
+            return "dashdot", "RandomForest log"
+
+
+def find_line_style(filename):
+    if "correlations" in filename:
+        return "dashed"
+    else:
+        return "solid"
+
+
+def find_line_color(filename):
+    if "alpha" in filename:
+        if "l2" in filename:
+            return "blue"
+        elif "log" in filename:
+            return "dodgerblue"
+    elif "beta" in filename:
+        if "l2" in filename:
+            return "#ff7f0e"
+        elif "log" in filename:
+            return "darkgoldenrod"
+    elif "delta" in filename:
+        if "l2" in filename:
+            return "darkgreen"
+        elif "log" in filename:
+            return "limegreen"
+    elif "gamma" in filename:
+        if "l2" in filename:
+            return "darkred"
+        elif "log" in filename:
+            return "#d62728"
+    elif "theta" in filename:
+        if "l2" in filename:
+            return "indigo"
+        elif "log" in filename:
+            return "mediumorchid"
+
+
 def plot_accuracies_wrt_parameter(path_tables, save_path, model_name, regularization, save_fig=False,
                                   y_scale=np.linspace(0, 1, 21, endpoint=True), all_epochs=False):
     path = path_tables
     all_files = Path(path).glob('*.csv')
     params = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.6499999999999999, 0.7, 0.75, 0.8,
               0.85, 0.9, 0.95, 1.]
-    # files = glob.glob(path, recursive=true)
     for file in all_files:
         df = pd.read_csv(file)
         x = df["alpha/beta"]
@@ -156,7 +108,7 @@ def plot_accuracies_wrt_parameter(path_tables, save_path, model_name, regulariza
         title = model_name + "_" + regularization + "_accuracies"
         if not condition and not all_epochs:
             if regularization in name_of_file:
-                plt.plot(x, y, label=find_labels(name_of_file))
+                plt.plot(x, y, label=find_band_labels(name_of_file))
                 plt.legend()
                 plt.title(title)
                 plt.legend()
@@ -185,7 +137,7 @@ def plot_accuracies_wrt_parameter(path_tables, save_path, model_name, regulariza
     plt.show()
 
 
-def best_accuracies_df_all_epochs(params, dataframe):
+def create_df_best_accuracies_all_epochs(params, dataframe):
     new_accuracy_table = pd.DataFrame(columns=['reg', 'band', 'alpha/beta', 'accuracy'])
     bands = ["alpha", "beta", "delta", "gamma", "theta"]
     for band in bands:
@@ -215,8 +167,7 @@ def plot_accuracies_wrt_parameter_from1file(path_tables, save_path, model_name, 
 
         if "all_epochs" in name_of_file:
             if regularization in name_of_file:
-                best_accuracies_df = best_accuracies_df_all_epochs(params=params, dataframe=df)
-
+                best_accuracies_df = create_df_best_accuracies_all_epochs(params=params, dataframe=df)
                 alpha_accuracy = best_accuracies_df.loc[best_accuracies_df["band"] == "alpha"]
                 alpha_accuracy = alpha_accuracy['accuracy']
                 alpha_accuracy = alpha_accuracy.to_numpy()
@@ -250,3 +201,36 @@ def plot_accuracies_wrt_parameter_from1file(path_tables, save_path, model_name, 
                 if save_fig:
                     plt.savefig(save_path + title + ".png")
                 plt.show()
+
+
+def plot_accuracies_all_settings_1band(path_tables, save_path, band, save_fig=False,
+                                       y_scale=np.linspace(0, 1, 21, endpoint=True)):
+    path = os.path.join(path_tables, '**/*.csv')
+    all_files = glob.glob(path, recursive=True)
+    params = [0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.6499999999999999, 0.7, 0.75, 0.8,
+              0.85, 0.9, 0.95, 1.]
+    for file in all_files:
+        name_of_file = os.path.basename(os.path.normpath(file))
+        name_of_file = name_of_file[:-4]
+        condition = "all_epochs" in name_of_file
+        title = band + "_all_settings_accuracies"
+        if band in name_of_file and not condition:
+            print(name_of_file)
+            df = pd.read_csv(file)
+            x = df["alpha/beta"]
+            y = df["accuracy"]
+            linestyle, label = find_reg_labels(name_of_file)
+            plt.plot(x, y, label=label, linestyle=find_line_style(name_of_file), color=find_line_color(name_of_file))
+            plt.legend()
+            plt.title(title)
+            plt.ylabel('accuracy')
+            plt.xlabel('learning graph parameter')
+            plt.xticks(params, rotation=90)
+            plt.yticks(y_scale)
+            plt.grid(True)
+            plt.tight_layout()
+
+    if save_fig:
+        plt.savefig(save_path + title + ".png")
+    plt.show()
+
